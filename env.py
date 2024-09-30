de137eaffd038ce83b72d15d32f6c9bba81b753b
Stuart Kellock <stuartkellock@gmail.com>
2024-09-30 13:08:17 +0000

    corrected contact form issues

---
+++ env.py
@@ -0,0 +1,3 @@
+import os
+
+os.environ.setdefault("SECRECT_KEY", "secret_flash_key")
--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 import os
-from flask import Flask, render_template
-
+import json
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py")
+    import env
 
 app = Flask(__name__)
 
@@ -15,9 +17,17 @@ def about():
     return render_template("about.html", page_title="About")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
+
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html", page_title="Careers")
 
 
 if __name__ == "__main__":
