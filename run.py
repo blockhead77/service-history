0f96682b698e2fa2cc767c7b175cbeff6dbe84f3
Stuart Kellock <stuartkellock@gmail.com>
2024-09-30 10:38:07 +0000

    Updated Contact Form and JS Links in Base Template

--- run.py
+++ run.py
@@ -12,17 +12,12 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", page_title="About")
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
-
-
-@app.route("/history")
-def history():
-    return render_template("history.html")
+    return render_template("contact.html", page_title="Contact")
 
 
 if __name__ == "__main__":
